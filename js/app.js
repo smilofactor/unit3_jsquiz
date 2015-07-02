45a86c76bc308f88a5feb687ae65abf9d7e0338f
Steve M <smilovidov@gmail.com>
2015-07-02 10:25:17 -0400

    Variable refactoring in app.js, changed Check Answer button to Submit Answer

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 "use strict";
 $(document).ready(function() {
 
-
 var directionID,
 
 question_array = [{
@@ -26,7 +25,9 @@ configMap = {
   $IDquestion_header: $('#question_header'),
   $setH2: $('h2'),
   listItems_html: "<li><input type='radio' name='answerclick' class='input_answer' style='list-style-type: none' data-question-id="
-};
+},
+
+questionObject = {};
 
 function setPage(pageNum) {
 	configMap.$IDquestion_header.data('page-num', pageNum).attr('data-page-num', pageNum);
@@ -37,8 +38,10 @@ function clearAnswerList(pageNum){
 };
 
 function questionsLoop(pageNum) {
-    var questionID = pageNum - 1;
-    var questionArray = question_array[questionID];
+    var
+    questionID = pageNum - 1,
+    questionArray = question_array[questionID];
+  
     configMap.$setH2.text(questionArray.question + ' in loop');
 
     for (var i = 0; i < questionArray.allanswers.length; i++) {
@@ -49,58 +52,59 @@ function questionsLoop(pageNum) {
 
 
 function pageCount(directionID) {
-	var pageLimit = question_array.length;
-    var buttonDirect = directionID.buttonID;
-    var pageNum = directionID.pageID;
+	var 
+  pageLimit = question_array.length,
+  buttonDirect = directionID.buttonID,
+  pageNum = directionID.pageID;
 
     if (pageNum === 0 && buttonDirect === 'back') {
 
+      //Doing something
+      //Had an alert stating:
+      //already start at quiz
+      
     } else if (buttonDirect === 'forward') {
+      
       if (pageNum < pageLimit) {
-
         pageNum += 1;
         setPage(pageNum);
         clearAnswerList(pageNum);
-
-      } else {
       
-        alert("Quiz over");
-      
-      }
+        } else {
+        
+          //alert("Quiz over"); 
+          alert(JSON.stringify(questionObject));
+               
+        }
 
     } else {
 
       pageNum -= 1;
       setPage(pageNum);
 
-      if (pageNum > 0) {
-
-        clearAnswerList(pageNum);
-
-      } else {
-
+      if (pageNum > 0) { clearAnswerList(pageNum); } 
+      
+      else {
+        
         configMap.$setH2.text('Introduction');
         configMap.$IDanswer_list.find('li').remove();
-      
+        
       }
+      
     };
-
   };
 
-
   $('.direction_button').on('click', function() {
-    var buttonID = $(this).data('buttonid');
-    var pageID = configMap.$IDquestion_header.data('page-num');
+    var buttonID = $(this).data('buttonid'),
+    pageID = configMap.$IDquestion_header.data('page-num');
 
     directionID = ({
       pageID: pageID,
       buttonID: buttonID
     });
     pageCount(directionID);
-
   });
-
- 
+  
 var parsedIDConstruct = function(answerID, questionID) {
 	this.answerID = answerID;
 	this.questionID = questionID;
@@ -109,25 +113,22 @@ var parsedIDConstruct = function(answerID, questionID) {
 
 parsedIDConstruct.prototype = {
 	inputIDValues: function() {
-     var inputID = $('input[type=radio]:checked').data('question-id');  
-      var VID = inputID.split("_");
-      this.answerID = parseInt(VID[0], 10);
-      this.questionID = parseInt(VID[1], 10);
+    var
+     inputID = $('input[type=radio]:checked').data('question-id'),
+     VID = inputID.split("_");
+    
+      this.answerID = parseInt(VID[1], 10);
+      this.questionID = parseInt(VID[0], 10);
 	}
-
 }
 
-
-  $('.check_answer').on('click', function() {
+ $('.submit_answer').on('click', function() {
 
     var parsedID = new parsedIDConstruct();
     parsedID.inputIDValues();
-
-    var answerID = parsedID.answerID;
-    var questionID = parsedID.questionID;
-
-    var arrayAnswer = question_array[questionID].allanswers[answerID].correct;
-    alert(answerID + " " + arrayAnswer);
+    var answerID = parsedID.answerID,
+        questionID = parsedID.questionID;
+    questionObject[questionID] = answerID;
 
   });
 
